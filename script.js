842132ebb37f5a8381f0c038aeb4364eb607e91f
git status <215340863+g-sts@users.noreply.github.com>
2025-06-24 02:05:06 -0300

    Update index.html and style.css and script.js

--- script.js
+++ script.js
@@ -12,25 +12,32 @@ button.addEventListener('click', calcularImc)
 
 function calcularImc(){
 
+    if(nome.value === '' || altura.value === '' || peso.value === ''){
+         alert("Preencha todos os campos com valores validos");
+    } else{
+        imc = peso.value / ((altura.value/100) * (altura.value/100))
 
-    imc = peso.value / (altura.value * altura.value)
+        let res = ''
 
-    let res = ''
+        if(imc < 18.5){
+            res = 'abaixo do peso!'
+        }
+        else if(imc >= 18.5 && imc <= 24.9){
+            res = 'com o peso ideal.'
+        }
+        else if(imc >= 25 && imc <= 29.9){
+            res = 'um pouco acima do peso ideal!'
+        }
+        else if(imc > 30){
+            res = 'na faixa da obesidade!'
+        }
 
-    if(imc < 18.5){
-        res = 'abaixo do peso!'
-    }
-    else if(imc >= 18.5 && imc <= 24.5){
-        res = 'com o peso ideal.'
-    }
-    else if(imc >= 25 && imc <= 29.9){
-        res = 'um pouco acima do peso ideal!'
-    }
-    else if(imc > 30){
-        res = 'na faixa da obesidade!'
+        display.innerHTML = `${nome.value} seu IMC é ${imc.toFixed(2)} e você está ${res}`
     }
 
-    display.innerHTML = `${nome.value} seu IMC é ${imc} e você está ${res}`
+    nome.value = ''
+    altura.value = ''
+    peso.value = ''
 }
     
 
