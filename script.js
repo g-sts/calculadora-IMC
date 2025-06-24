5af7d77438f3d84127f63eecd937a9b884a5a3b7
git status <215340863+g-sts@users.noreply.github.com>
2025-06-24 01:27:23 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+let nome = document.querySelector('#inome')
+let altura = document.querySelector('#ialtura')
+let peso = document.querySelector('#ipeso')
+let display = document.querySelector('#display')
+
+let imc 
+
+
+
+button.addEventListener('click', calcularImc)
+
+function calcularImc(){
+
+
+    imc = peso.value / altura.value * altura.value
+
+    let res = ''
+
+    if(imc < 18.5){
+        res = 'abaixo do peso!'
+    }
+    else if(imc >= 18.5 && imc <= 24.5){
+        res = 'com o peso ideal.'
+    }
+    else if(imc >= 25 && imc <= 29.9){
+        res = 'um pouco acima do peso ideal!'
+    }
+    else if(imc > 30){
+        res = 'na faixa da obesidade!'
+    }
+
+    display.innerHTML = `${nome} seu IMC é ${imc} e você está ${res}`
+}
+    
+
+
+
+
